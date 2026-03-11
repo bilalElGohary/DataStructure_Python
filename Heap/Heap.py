4e0e9be3f083f0c8d220bb646baec3ceabcf985c
bilalElGohary <bilalelgohary302@gmail.com>
2026-03-11 06:07:31 +0200

    Finshed Heap/Heap.py

--- Heap/Heap.py
+++ Heap/Heap.py
@@ -2,43 +2,82 @@ class MinHeap:
   def __init__(self):
     self.heap = []
     
-  # runtime: 
   def __len__(self):
-    pass
+    return len(self.heap)
     
-  # runtime: 
   def __repr__(self):
-    pass
+    return str(self.heap)
     
-  # runtime: 
+  # runtime: O(log n) 
   def insert(self, key, value):
-    pass
+    self.heap.append((key, value))
+    self._siftUp(len(self.heap) - 1)
   
-  # runtime: 
+  # runtime: O(1)
   def peekMin(self):
-    pass
+    if not self.heap:
+      raise IndexError("The heap is empty.")
+    return self.heap[0]
   
-  # runtime: 
+  # runtime: O(log n)
   def extractMin(self):
-    pass
+    if not self.heap:
+      raise IndexError("The heap is empty.")
+    min = self.heap[0]
+    last = self.heap.pop()
+    if self.heap:
+      self.heap[0] = last
+      self._siftDown(0)
+    return min
   
-  # runtime: 
+  # runtime: O(n)
   def heapify(self, elements):
-    pass
+    self.heap = list(elements)
+    for i in reversed(range(self._parent(len(self.heap) - 1) + 1)):
+      self._siftDown(i)
   
-  # runtime: 
+  # runtime: O(n)
   def meld(self, otherHeap):
-    pass
+    combined = self.heap + otherHeap
+    self.heapify(combined)
+    otherHeap.heap = []
   
   # helper methods
   def _parent(self, index):
-    pass
+    return (index + 1) // 2 if index != 0 else None
   
   def _left(self, index):
-    pass
+    left = 2 * index + 1
+    return left if left < len(self.heap) else None
   
   def _right(self, index):
-    pss
+    right = 2 * index + 2
+    return right if right < len(self.heap) else None
+
   
+  def _siftUp(self, index):
+    # swim
+    parent = self._parent(index)
+    while parent is not None and self.heap[index][0] < self.heap[parent][0]:
+      self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
+      index = parent
+      parent = self._parent(index)
+  
+  def _siftDown(self, index):
+    # sink
+    while True:
+      smallest = index
+      left = self._left
+      right = self._right
+      if left is not None and self.heap[left][0] < self.heap[smallest][0]:
+        smallest = left
+      if right is not None and self.heap[right][0] < self.heap[smallest][0]:
+        smallest = right
+      if smallest == index:
+        break
+    self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+    index = smallest
+
+    
 if __name__ == "__main__":
   pass
