b74764d07b2e034ec9fb9e7475cf9532e96adc21
bilalElGohary <bilalelgohary302@gmail.com>
2026-03-16 02:10:06 +0200

    structure: Trie/Trie.py

---
+++ Trie/Trie.py
@@ -0,0 +1,39 @@
+class Node:
+  def __init__(self):
+    self.children = dict()
+    self.isEndWord = False
+  
+class Trie:
+  def __int__(self):
+    self.root = False
+  
+  # runtime: 
+  def __repr__(self):
+    pass
+
+  # runtime: 
+  def insert(self, word):
+    pass
+  
+  # runtime: 
+  def search(self, word):
+    pass
+  
+  # runtime: 
+  def delete(self, word):
+    pass
+  
+  # runtime: 
+  def hasPrefix(self, prefix):
+    pass
+
+  # runtime: 
+  def startWith(self, prefix):
+    pass
+
+  # runtime: 
+  def listWord(self):
+    pass
+  
+if __name__ == "__main__":
+  pass
